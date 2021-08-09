337ef7b618fdff1d5f02d819bf630106fcc2eb83
spanishvectorproject <88663569+spanishvectorproject@users.noreply.github.com>
2021-08-09 14:29:19 +0200

    Add files via upload

---
+++ spacejam.js
@@ -0,0 +1,36 @@
+function overlayOn1() {
+    document.getElementById("overlaySerie").style.display = "block";
+}
+function overlayOff1() {
+    document.getElementById("overlaySerie").style.display = "none";
+}
+function overlayOn2() {
+    document.getElementById("overlaySerie1").style.display = "block";
+}
+function overlayOff2() {
+    document.getElementById("overlaySerie1").style.display = "none";
+}
+function overlayOn3() {
+    document.getElementById("overlaySerie2").style.display = "block";
+}
+function overlayOff3() {
+    document.getElementById("overlaySerie2").style.display = "none";
+}
+function overlayOn4() {
+    document.getElementById("overlaySerie3").style.display = "block";
+}
+function overlayOff4() {
+    document.getElementById("overlaySerie3").style.display = "none";
+}
+function overlayOn5() {
+    document.getElementById("overlaySerie4").style.display = "block";
+}
+function overlayOff5() {
+    document.getElementById("overlaySerie4").style.display = "none";
+}
+function overlayOn6() {
+    document.getElementById("overlaySerie5").style.display = "block";
+}
+function overlayOff6() {
+    document.getElementById("overlaySerie5").style.display = "none";
+}
